d5d528e8e266a62392c254b64040f679581fa9e3
Dian Xiao <dxiao@andrew.cmu.edu>
2011-02-27 15:41:21 -0500

    Basic scaffolding

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PartyManager::Application.config.secret_token = 'be77d9288a7a364fd3cbe55db7313b59cab7c26726ad2c72728615024d50ce748cdff731103d7f62e47581362569cf736e66c5dcf29b6de6a829506041e85815'
